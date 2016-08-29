8e50fac8cebf1b6a7bf82833b82325e316061fbd
Daniele Basile <asterix@develer.com>
2016-08-29 14:46:24 +0200

    Add heartbead for board discovery.

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 )
 
 // ConnPort Default listen port
 const ConnPort = "20000"
 const header = "Hello from TestBoard Suite"
 const version = "0.1.0"
+const hwType = "TestBoard"
 
 func sendReply(conn net.Conn, code int, msg string) {
 	c := strconv.Itoa(code)
@@ -61,10 +64,47 @@ func handleRequest(conn net.Conn) {
 	conn.Close()
 }
 
+func heartbeatLoop(beat net.Conn) {
+
+	ip := beat.LocalAddr()
+	ipAddr := strings.Split(ip.String(), ":")[0]
+
+	macStr := ""
+	ifs, err := net.Interfaces()
+	if err != nil {
+		macStr = ""
+	} else {
+		macStr = fmt.Sprintf("%s", ifs[1].HardwareAddr)
+	}
+
+	fmt.Println("Start server on: ", ipAddr, macStr)
+	heartbeatStr := fmt.Sprintf("%s %s %s %s 0 2", ipAddr, macStr, hwType, version)
+
+	for {
+		_, err := beat.Write([]byte(heartbeatStr))
+		if err != nil {
+			fmt.Println(err)
+		}
+		time.Sleep(time.Second * 1)
+	}
+}
+
 func main() {
 
 	// Init commands table
 	InitCmd()
+
+	ServerAddr, err := net.ResolveUDPAddr("udp", "255.255.255.255:23001")
+	if err != nil {
+		fmt.Printf("ResolveUDPAddr error %v", err)
+	}
+	beat, err := net.DialUDP("udp", nil, ServerAddr)
+	if err != nil {
+		fmt.Printf("DialUDP error %v", err)
+		panic(err)
+	}
+	defer beat.Close()
+	go heartbeatLoop(beat)
 
 	// Listen for incoming connections.
 	l, err := net.Listen("tcp", "0.0.0.0"+":"+ConnPort)
